876bdfe3d1f6d8b41b7bb23954727225e664b48b
robhoeppner <robhoeppner@gmail.com>
2023-01-16 08:20:19 +0100

    day 6

---
+++ day6/b.ts
@@ -0,0 +1,18 @@
+const input = await Deno.readTextFile("./input.txt");
+
+const chars = input.split("");
+
+let index = -1;
+
+for (let i = 0; i < chars.length; i++) {
+  const seq = chars.slice(i, i + 14);
+  let repeat = false;
+
+  seq.forEach((char, i) => {
+    if (seq.lastIndexOf(char) !== i) repeat = true;
+  });
+
+  if (!repeat && index < 0) index = i;
+}
+
+console.log(index + 14);
